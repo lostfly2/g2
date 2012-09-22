92aa9b7bc86de8faa1f17b1b8bc9b5ad1a395373
Jordi Pijuan <jordi@pijuan.info>
2012-09-22 12:25:36 +0200

    Modificació test2

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 /*
  *---------------------------------------------------------------
- * APPLICATION ENVIRONMENT
+ * APPLICATION ENVIRONMENT211212
  *---------------------------------------------------------------
  *
  * You can load different configurations depending on your
